f638935d7ddf0efaa3a7f96e12c2460e244a2085
Inteleger <kdz2314461602@gmail.com>
2026-07-15 17:59:19 +0800

    debugged preprocess.py and finished dataset.py

--- src/model.py
+++ src/model.py
@@ -3,25 +3,20 @@ import torch.nn as nn
 
 from config import *
 
-class ReviewAnalysisModel(nn.Module):
-    def __init__(self, vocab_size, padding_idx=0):
-        super(ReviewAnalysisModel, self).__init__()
-        self.embedding = nn.Embedding(vocab_size, embedding_dim=EMBEDDING_SIZE, padding_idx=padding_idx)
-        self.lstm = nn.LSTM(input_size=EMBEDDING_SIZE, hidden_size=HIDDEN_SIZE, batch_first=True)
-        self.linear = nn.Linear(in_features=HIDDEN_SIZE, out_features=1)
+from transformers import AutoModel
 
-    def forward(self, input):
-        embed = self.embedding(input)
-        output, _ = self.lstm(embed)
-        indices = torch.arange(output.shape[0])
-        lengths = (input != self.embedding.padding_idx).sum(dim=1)
-        features = output[indices, lengths - 1]
-        result = self.linear(features).squeeze(-1)
+class ReviewAnalysisModel(nn.Module):
+    def __init__(self):
+        super(ReviewAnalysisModel, self).__init__()
+        self.bert = AutoModel.from_pretrained(PRE_TRAINED_DIR/BERT_MODEL)
+        self.linear = nn.Linear(self.bert.config.hidden_size, 1)
+
+    def forward(self, input_ids, attention_mask, token_type_ids):
+        output = self.bert(input_ids, attention_mask, token_type_ids)
+        cls_hidden_stat = output.pooler_output
+        result = self.linear(cls_hidden_stat).squeeze(-1)
         return result
 
 if __name__ == '__main__':
-    vocab_size = 1000
-    input = torch.randint(1000, size=(64, 5))
-    model = ReviewAnalysisModel(vocab_size)
-    output = model(input)
-    print(output.size())
+    model = ReviewAnalysisModel()
+    print(model)
